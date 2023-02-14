666f9f46a807d94dfe7c6ad1e352c00b638f6dc2
Eggys321 <imonieroheguono@gmail.com>
2023-02-14 21:19:21 +0100

    firts commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import WelcomePage from './components/WelcomePage';
+import {Routes, Route} from 'react-router-dom'
+import RegisterPage from './components/RegisterPage';
+import VerifyEmail from './components/VerifyEmail';
+import RegSuccessPage from './components/RegSuccessPage';
+import SignIn from './components/SignIn';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<WelcomePage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/verify" element={<VerifyEmail/>}/>
+        <Route path="/reg" element={<RegSuccessPage/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
